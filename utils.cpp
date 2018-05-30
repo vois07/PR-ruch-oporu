ace7fa437cf32a9003a91556225885e327e74990
Paulina <paulina.lester27@gmail.com>
2018-05-30 12:03:54 +0200

    Added creation of MPI_UPDATE type

--- utils.cpp
+++ utils.cpp
@@ -10,9 +10,24 @@ namespace utils
 		return std::stoi(ans);
 	}
 	
-	struct Update
+	typedef struct Update_s
 	{
 		int meetingID;
 		int newParticipant;
-	};
+	}Update;
+	
+	void makeMPI_UPDATE()
+	{
+		const int nitems = 2;
+		int blocklengths[2]={1,1};
+		MPI_Datatype types[2]={MPI_INT, MPI_INT};
+		MPI_Datatype MPI_UPDATE;
+		MPI_Aint offsets[2];
+
+		offsets[0] = offsetof(Update,meetingID);
+		offsets[1] = offsetof(Update,newParticipant);
+
+		MPI_Type_create_struct(nitems, blocklengths, offsets, types, &MPI_UPDATE);
+		MPI_Type_commit(&MPI_UPDATE);
+	}
 }
